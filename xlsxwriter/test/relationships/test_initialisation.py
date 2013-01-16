96eb55815180c7b676c286926eea0d83d14f3f2f
John McNamara <jmcnamara@cpan.org>
2013-01-16 00:35:18 +0000

    Added XLSX Relationships class and tests.

---
+++ xlsxwriter/test/relationships/test_initialisation.py
@@ -0,0 +1,36 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013, John McNamara, jmcnamara@cpan.org
+#
+
+import unittest
+from StringIO import StringIO
+from ...relationships import Relationships
+
+
+class TestInitialisation(unittest.TestCase):
+    """
+    Test initialisation of the Relationships class and call a method.
+
+    """
+
+    def setUp(self):
+        self.fh = StringIO()
+        self.relationships = Relationships()
+        self.relationships._set_filehandle(self.fh)
+
+    def test_xml_declaration(self):
+        """Test Relationships xml_declaration()"""
+
+        self.relationships._xml_declaration()
+
+        exp = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n"""
+        got = self.fh.getvalue()
+
+        self.assertEqual(got, exp)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ xlsxwriter/test/relationships/test_relationships01.py
@@ -0,0 +1,52 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013, John McNamara, jmcnamara@cpan.org
+#
+
+import unittest
+from StringIO import StringIO
+from ..helperfunctions import _xml_to_list
+from ...relationships import Relationships
+
+
+class TestAssembleRelationships(unittest.TestCase):
+    """
+    Test assembling a complete Relationships file.
+
+    """
+    def test_assemble_xml_file(self):
+        """Test writing an Relationships file."""
+        self.maxDiff = None
+
+        fh = StringIO()
+        rels = Relationships()
+        rels._set_filehandle(fh)
+
+        rels._add_document_relationship('/worksheet', 'worksheets/sheet1.xml')
+        rels._add_document_relationship('/theme', 'theme/theme1.xml')
+        rels._add_document_relationship('/styles', 'styles.xml')
+        rels._add_document_relationship('/sharedStrings', 'sharedStrings.xml')
+        rels._add_document_relationship('/calcChain', 'calcChain.xml')
+
+        rels._assemble_xml_file()
+
+        exp = _xml_to_list("""
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+                  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
+                  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="theme/theme1.xml"/>
+                  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
+                  <Relationship Id="rId4" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/>
+                  <Relationship Id="rId5" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/calcChain" Target="calcChain.xml"/>
+                </Relationships>
+                """)
+
+        got = _xml_to_list(fh.getvalue())
+
+        self.assertEqual(got, exp)
+
+
+if __name__ == '__main__':
+    unittest.main()
