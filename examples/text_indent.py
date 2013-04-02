621a8854ab96bbdf214eec3171ab87c240113d26
John McNamara <jmcnamara@cpan.org>
2013-04-03 00:35:22 +0100

    Added text indent example.

---
+++ examples/text_indent.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# A simple formatting example using XlsxWriter.
+#
+# This program demonstrates the indentation cell format.
+#
+# Copyright 2013, John McNamara, jmcnamara@cpan.org
+#
+from xlsxwriter.workbook import Workbook
+
+workbook = Workbook('text_indent.xlsx')
+
+worksheet = workbook.add_worksheet()
+indent1 = workbook.add_format({'indent': 1})
+indent2 = workbook.add_format({'indent': 2})
+
+worksheet.set_column('A:A', 40)
+
+worksheet.write('A1', "This text is indented 1 level", indent1)
+worksheet.write('A2', "This text is indented 2 levels", indent2)
+
+workbook.close()
