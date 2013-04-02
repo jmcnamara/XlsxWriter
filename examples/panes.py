bca68a68e95e3cd696557efd74510122a867f9ab
John McNamara <jmcnamara@cpan.org>
2013-04-02 20:18:10 +0100

    Cleaned up panes example.

--- examples/panes.py
+++ examples/panes.py
@@ -19,12 +19,12 @@ worksheet4 = workbook.add_worksheet('Panes 4')
 # Set up some formatting and text to highlight the panes.
 #
 header_format = workbook.add_format({'bold': True,
-                                     'align': 'center_format',
+                                     'align': 'center',
                                      'valign': 'vcenter',
                                      'fg_color': '#D7E4BC',
                                      'border': 1})
 
-center_format = workbook.add_format({'align': 'center_format'})
+center_format = workbook.add_format({'align': 'center'})
 
 
 #######################################################################
@@ -40,12 +40,12 @@ worksheet1.set_selection('C3')
 
 
 # Some text to demonstrate scrolling.
-for i in range(0, 8 + 1):
-    worksheet1.write(0, i, 'Scroll down', header_format)
+for col in range(0, 9):
+    worksheet1.write(0, col, 'Scroll down', header_format)
 
-for i in range(1, 100 + 1):
-    for j in range(0, 8 + 1):
-        worksheet1.write(i, j, i + 1, center_format)
+for row in range(1, 100):
+    for col in range(0, 9):
+        worksheet1.write(row, col, row + 1, center_format)
 
 
 #######################################################################
@@ -59,13 +59,10 @@ worksheet2.set_column('A:A', 16)
 worksheet2.set_selection('C3')
 
 # Some text to demonstrate scrolling.
-for i in range(0, 49 + 1):
-    worksheet2.set_row(i, 15)
-    worksheet2.write(i, 0, 'Scroll right', header_format)
-
-for i in range(0, 49 + 1):
-    for j in range(1, 25 + 1):
-        worksheet2.write(i, j, j, center_format)
+for row in range(0, 50):
+    worksheet2.write(row, 0, 'Scroll right', header_format)
+    for col in range(1, 26):
+        worksheet2.write(row, col, col, center_format)
 
 
 #######################################################################
@@ -76,19 +73,18 @@ worksheet3.freeze_panes(1, 1)
 
 # Other sheet formatting.
 worksheet3.set_column('A:Z', 16)
+worksheet3.set_row(0, 20)
 worksheet3.set_selection('C3')
 worksheet3.write(0, 0, '', header_format)
 
 # Some text to demonstrate scrolling.
-for i in range(1, 25 + 1):
-    worksheet3.write(0, i, 'Scroll down', header_format)
+for col in range(1, 26):
+    worksheet3.write(0, col, 'Scroll down', header_format)
 
-for i in range(1, 49 + 1):
-    worksheet3.write(i, 0, 'Scroll right', header_format)
-
-for i in range(1, 49 + 1):
-    for j in range(1, 25 + 1):
-        worksheet3.write(i, j, j, center_format)
+for row in range(1, 50):
+    worksheet3.write(row, 0, 'Scroll right', header_format)
+    for col in range(1, 26):
+        worksheet3.write(row, col, col, center_format)
 
 
 #######################################################################
@@ -104,14 +100,12 @@ worksheet4.split_panes(15, 8.43)
 worksheet4.set_selection('C3')
 
 # Some text to demonstrate scrolling.
-for i in range(1, 25 + 1):
-    worksheet4.write(0, i, 'Scroll', center_format)
+for col in range(1, 26):
+    worksheet4.write(0, col, 'Scroll', center_format)
 
-for i in range(1, 49 + 1):
-    worksheet4.write(i, 0, 'Scroll', center_format)
-
-for i in range(1, 49 + 1):
-    for j in range(1, 25 + 1):
-        worksheet4.write(i, j, j, center_format)
+for row in range(1, 50):
+    worksheet4.write(row, 0, 'Scroll', center_format)
+    for col in range(1, 26):
+        worksheet4.write(row, col, col, center_format)
 
 workbook.close()
