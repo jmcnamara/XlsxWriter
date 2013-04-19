1ef21e99180059800247a2cd0d3c0db5d6d6f13c
John McNamara <jmcnamara@cpan.org>
2013-04-19 23:17:03 +0100

    Added tests for chart sparse and string data.

---
+++ xlsxwriter/test/comparison/test_chart_str02.py
@@ -0,0 +1,85 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013, John McNamara, jmcnamara@cpan.org
+#
+
+import unittest
+import os
+from ...workbook import Workbook
+from ..helperfunctions import _compare_xlsx_files
+
+
+class TestCompareXLSXFiles(unittest.TestCase):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'chart_str02.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file when the chart datacontains strings."""
+        filename = self.got_filename
+
+        ####################################################
+
+        workbook = Workbook(filename)
+
+        worksheet = workbook.add_worksheet()
+        chart = workbook.add_chart({'type': 'scatter'})
+
+        chart.axis_ids = [41671680, 41669376]
+
+        data = [
+            [1, 2, 'Foo', 4, 5],
+            [2, 4, 'Bar', 8, 10],
+            [3, 6, 'Baz', 12, 15],
+
+        ]
+
+        worksheet.write_column('A1', data[0])
+        worksheet.write_column('B1', data[1])
+        worksheet.write_column('C1', data[2])
+
+        chart.add_series({
+            'categories': '=Sheet1!$A$1:$A$5',
+            'values': '=Sheet1!$B$1:$B$5',
+        })
+
+        chart.add_series({
+            'categories': '=Sheet1!$A$1:$A$5',
+            'values': '=Sheet1!$C$1:$C$5',
+        })
+        worksheet.insert_chart('E9', chart)
+
+        workbook.close()
+
+        ####################################################
+
+        got, exp = _compare_xlsx_files(self.got_filename,
+                                       self.exp_filename,
+                                       self.ignore_files,
+                                       self.ignore_elements)
+
+        self.assertEqual(got, exp)
+
+    def tearDown(self):
+        # Cleanup.
+        if os.path.exists(self.got_filename):
+            os.remove(self.got_filename)
+
+
+if __name__ == '__main__':
+    unittest.main()
