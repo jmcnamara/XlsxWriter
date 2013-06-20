09e2710e530b04c4e5bc94fa02be5ae33e6aba96
Dieter Vandenbussche <dieterv77@gmail.com>
2013-06-19 20:01:24 -0400

    Remove subtype stuff for line charts

--- xlsxwriter/chart_line.py
+++ xlsxwriter/chart_line.py
@@ -31,11 +31,6 @@ class ChartLine(chart.Chart):
         if options is None:
             options = {}
 
-        self.subtype = options.get('subtype')
-
-        if not self.subtype:
-            self.subtype = 'straight'
-
         self.default_marker = {'type': 'none'}
         self.smooth_allowed = True
 
@@ -66,8 +61,6 @@ class ChartLine(chart.Chart):
 
         if not len(series):
             return
-
-        self._modify_series_formatting()
 
         self._xml_start_tag('c:lineChart')
 
@@ -112,41 +105,3 @@ class ChartLine(chart.Chart):
         self._xml_end_tag('c:marker')
 
         self._xml_end_tag('c:dPt')
-
-    def _modify_series_formatting(self):
-        # Add default formatting to the series data unless it has already been
-        # specified by the user.
-        subtype = self.subtype
-
-        # The default scatter style "markers only" requires a line type.
-        if subtype == 'marker_only':
-
-            # Go through each series and define default values.
-            for series in self.series:
-
-                # Set a line type unless there is already a user defined type.
-                if not series['line']['defined']:
-                    series['line'] = {'width': 2.25,
-                                      'none': 1,
-                                      'defined': 1,
-                                      }
-
-        # Turn markers on for subtypes that have them.
-        if 'marker' in subtype:
-
-            # Go through each series and define default values.
-            for series in self.series:
-                # Set a marker type unless there is a user defined type.
-                if series['marker'] is None or not series['marker']['defined']:
-                    series['marker'] = {'type': 'automatic',
-                                        'automatic': 1,
-                                        'defined': 1,
-                                        'line': self._get_line_properties(None),
-                                        'fill': self._get_fill_properties(None)
-                                        }
-
-        # Turn on smoothing if required
-        if 'smooth' in subtype:
-            for series in self.series:
-                if series['smooth'] is None:
-                    series['smooth'] = True
