51f209ae77f003c313bfd0849eebee6655a5dfcc
John McNamara <jmcnamara@cpan.org>
2013-06-28 01:17:02 +0100

    Prep for release 0.3.5

--- dev/docs/source/conf.py
+++ dev/docs/source/conf.py
@@ -49,7 +49,7 @@ copyright = u'2013, John McNamara'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.4'
+version = '0.3.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
