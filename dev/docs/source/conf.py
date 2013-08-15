473fb514056b89d86840c19313ae7726daafae37
John McNamara <jmcnamara@cpan.org>
2013-08-16 00:47:27 +0100

    Prep for release 0.3.7

--- dev/docs/source/conf.py
+++ dev/docs/source/conf.py
@@ -49,7 +49,7 @@ copyright = u'2013, John McNamara'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.6'
+version = '0.3.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- xlsxwriter/__init__.py
+++ xlsxwriter/__init__.py
@@ -1,2 +1,2 @@
-__VERSION__ = '0.3.6'
+__VERSION__ = '0.3.7'
 from .workbook import Workbook
