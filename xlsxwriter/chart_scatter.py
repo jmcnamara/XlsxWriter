ca54fa8663401e0ec560524c65c8461da42a94b6
John McNamara <jmcnamara@cpan.org>
2013-10-16 23:18:07 +0100

    Fixed issue when applying markers to straight scatter chart.
    Close #54.

--- xlsxwriter/chart_scatter.py
+++ xlsxwriter/chart_scatter.py
@@ -274,10 +274,8 @@ class ChartScatter(chart.Chart):
             # Go through each series and define default values.
             for series in self.series:
                 # Set a marker type unless there is a user defined type.
-                if series['marker'] is None or not series['marker']['defined']:
-                    series['marker'] = {'type': 'none',
-                                        'defined': 1,
-                                        }
+                if series.get('marker') or not series['marker']['defined']:
+                    series['marker'] = {'type': 'none', 'defined': 1}
 
     def _write_d_pt_point(self, index, point):
         # Write an individual <c:dPt> element. Override the parent method to
