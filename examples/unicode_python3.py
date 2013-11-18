25d32c5d4634927f0c28af2a3dedcc4640c3ca82
John McNamara <jmcnamara@cpan.org>
2013-11-18 23:20:28 +0000

    Added Python 2/3 Unicode examples.

---
+++ examples/unicode_python3.py
@@ -0,0 +1,22 @@
+###############################################################################
+#
+#
+# A simple Unicode spreadsheet in Python 3 using the XlsxWriter Python module.
+#
+# Copyright 2013, John McNamara, jmcnamara@cpan.org
+#
+
+# To write Unicode text in UTF-8 to a xlsxwriter file in Python 3:
+#
+# 1. Encode the file at UTF-8.
+#
+#
+
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('unicode_python3.xlsx')
+worksheet = workbook.add_worksheet()
+
+worksheet.write('B3', 'Это фраза на русском!')
+
+workbook.close()
