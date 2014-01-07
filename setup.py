f1f71192978715b1d9d57d598a10da6aa4511ccb
John McNamara <jmcnamara@cpan.org>
2014-01-07 22:43:48 +0000

    Added categories to setup.py.

--- setup.py
+++ setup.py
@@ -10,4 +10,16 @@ setup(
     license='BSD',
     description='A Python module for creating Excel XLSX files.',
     long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
