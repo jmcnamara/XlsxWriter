1572c2e7a3cb54715b79e0305697ea6b9bca6376
John McNamara <jmcnamara@cpan.org>
2014-02-21 00:36:50 +0000

    Added testcase for contiguous merged cells.

---
+++ xlsxwriter/test/comparison/test_merge_cells01.py
@@ -0,0 +1,68 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013, John McNamara, jmcnamara@cpan.org
+#
+
+import unittest
+import os
+from ...workbook import Workbook
+from ..helperfunctions import _compare_xlsx_files
+
+
+class TestCompareXLSXFiles(unittest.TestCase):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'merge_cells01.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file."""
+        filename = self.got_filename
+
+        ####################################################
+
+        workbook = Workbook(filename)
+
+        worksheet = workbook.add_worksheet()
+        format = workbook.add_format({'align': 'center'})
+
+        worksheet.set_selection('A4')
+
+        worksheet.merge_range('A1:A2', 'col1', format)
+        worksheet.merge_range('B1:B2', 'col2', format)
+        worksheet.merge_range('C1:C2', 'col3', format)
+        worksheet.merge_range('D1:D2', 'col4', format)
+
+        workbook.close()
+
+        ####################################################
+
+        got, exp = _compare_xlsx_files(self.got_filename,
+                                       self.exp_filename,
+                                       self.ignore_files,
+                                       self.ignore_elements)
+
+        self.assertEqual(got, exp)
+
+    def tearDown(self):
+        # Cleanup.
+        if os.path.exists(self.got_filename):
+            os.remove(self.got_filename)
+
+
+if __name__ == '__main__':
+    unittest.main()
