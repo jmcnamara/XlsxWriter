94e5f5f8e64332c8a7b13409a4d23b0b3040153d
John McNamara <jmcnamara@cpan.org>
2014-11-15 17:53:16 +0000

    Prep for release 0.6.4

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
     from distutils.core import setup
 
 if sys.version_info < (2, 5, 0):
-    warn("The minimum Python version supported by XlsxWriter is 2.5.0.")
+    warn("The minimum Python version supported by XlsxWriter is 2.5.1.")
     exit()
 
 
 setup(
     name='XlsxWriter',
-    version='0.6.3',
+    version='0.6.4',
     author='John McNamara',
     author_email='jmcnamara@cpan.org',
     url='https://github.com/jmcnamara/XlsxWriter',
