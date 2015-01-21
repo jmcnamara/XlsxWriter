0154fbb55e2601d87cbf7ebe492604a4f395f1ba
John McNamara <jmcnamara@cpan.org>
2015-01-21 21:03:30 +0000

    Test for vertical and horizontal centered format.

---
+++ xlsxwriter/test/comparison/test_format11.py
@@ -0,0 +1,46 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2015, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'format11.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test a vertical and horizontal centered format."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        centered = workbook.add_format({
+            'align': 'center',
+            'valign': 'vcenter'
+        })
+
+        worksheet.write('B2', 'Foo', centered)
+
+        workbook.close()
+
+        self.assertExcelEqual()
