5e7448918328daebae1420ee5881ad700f1dd71e
John McNamara <jmcnamara@cpan.org>
2015-03-02 22:43:45 +0000

    Added test for border formats.

---
+++ xlsxwriter/test/comparison/test_format12.py
@@ -0,0 +1,59 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2015, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'format12.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test a vertical and horizontal centered format."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        top_left_bottom = workbook.add_format({
+            'left': 1,
+            'top': 1,
+            'bottom': 1,
+        })
+
+        top_bottom = workbook.add_format({
+            'top': 1,
+            'bottom': 1,
+        })
+
+        top_left = workbook.add_format({
+            'left': 1,
+            'top': 1,
+        })
+
+        worksheet.write('B2', 'test', top_left_bottom)
+        worksheet.write('D2', 'test', top_left)
+        worksheet.write('F2', 'test', top_bottom)
+
+        workbook.close()
+
+        self.assertExcelEqual()
