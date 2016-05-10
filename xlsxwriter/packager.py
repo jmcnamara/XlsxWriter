d75d85445aa8cefd28c11a1477d88d66a356be59
John McNamara <jmcnamara@cpan.org>
2016-05-10 23:25:34 +0100

    Fix for handling readonly images on Windows.
    Issue #352

--- xlsxwriter/packager.py
+++ xlsxwriter/packager.py
@@ -7,6 +7,7 @@
 
 # Standard packages.
 import os
+import stat
 import tempfile
 from shutil import copy
 
@@ -584,6 +585,12 @@ class Packager(object):
                 else:
                     copy(filename, os_filename)
 
+                    # Allow copies of Windows readonly images to be deleted.
+                    try:
+                        os.chmod(os_filename,
+                                 os.stat(os_filename).st_mode | stat.S_IWRITE)
+                    except:
+                        pass
             else:
                 # For in-memory mode we read the image into a stream.
                 if image_data:
