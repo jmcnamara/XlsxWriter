03acdc5e06df8fe6cebf6d478cbb17babe99b926
John McNamara <jmcnamara@cpan.org>
2016-05-14 18:03:53 +0100

    Added test for full width number strings.

---
+++ xlsxwriter/test/comparison/test_utf8_11.py
@@ -0,0 +1,42 @@
+###############################################################################
+# _*_ coding: utf-8
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2016, John McNamara, jmcnamara@cpan.org
+#
+from __future__ import unicode_literals
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'utf8_11.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test the creation of an XlsxWriter file with utf-8 strings."""
+
+        workbook = Workbook(self.got_filename, {'strings_to_urls': True})
+
+        worksheet = workbook.add_worksheet()
+
+        worksheet.write('A1', '１２３４５')
+
+        workbook.close()
+
+        self.assertExcelEqual()
