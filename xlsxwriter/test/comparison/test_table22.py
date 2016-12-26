6a05aef93a7ce236e8e7d9b8cce2e26e623db1b2
John McNamara <jmcnamara@cpan.org>
2016-12-26 00:05:39 +0000

    Add test for table without header row.
    Issue #405.

---
+++ xlsxwriter/test/comparison/test_table22.py
@@ -0,0 +1,48 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2016, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'table22.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file with tables."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        data = [
+            ['apple', 'pie'],
+            ['pine', 'tree'],
+        ]
+
+        worksheet.set_column('B:C', 10.288)
+
+        worksheet.add_table('B2:C3', {'data': data, 'header_row': False})
+
+        workbook.close()
+
+        self.assertExcelEqual()
