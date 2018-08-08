2596998a82f7846142cb602001fb47f4c66e91ac
percious <chris@percious.com>
2018-08-08 16:00:17 -0600

    don't use isinstance for simple datetimes

--- xlsxwriter/worksheet.py
+++ xlsxwriter/worksheet.py
@@ -7,6 +7,7 @@
 
 # Standard packages.
 import codecs
+import datetime
 import os
 import re
 import sys
@@ -420,6 +421,11 @@ class Worksheet(xmlwriter.XMLwriter):
             return self.write_token_as_string(token, row, col, *args)
         if token_type is unicode:
             return self.write_token_as_string(str(token), row, col, *args)
+        if token_type in (datetime.datetime,
+                          datetime.date,
+                          datetime.time,
+                          datetime.timedelta):
+            return self.write_datetime(row, col, *args)
 
         # resort to isinstance for developers who have subclassed a primitive
         # Write boolean types.
