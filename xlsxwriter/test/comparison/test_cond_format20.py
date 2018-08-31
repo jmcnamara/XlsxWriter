7d504cc2d8271337389cd2b8df6e4b1f1b26faba
John McNamara <jmcnamara@cpan.org>
2018-09-01 00:20:42 +0100

    Test for issue with icon conditional formats with zero value
    Issue #565

---
+++ xlsxwriter/test/comparison/test_cond_format20.py
@@ -0,0 +1,51 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2018, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+        self.maxDiff = None
+
+        filename = 'cond_format20.xlsx'
+
+        test_dir = 'xlsxwriter/test/comparison/'
+        self.got_filename = test_dir + '_test_' + filename
+        self.exp_filename = test_dir + 'xlsx_files/' + filename
+
+        self.ignore_files = []
+        self.ignore_elements = {}
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file with conditionalformatting."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        worksheet.write('A1', 10)
+        worksheet.write('A2', 20)
+        worksheet.write('A3', 30)
+        worksheet.write('A4', 40)
+
+        worksheet.conditional_format('A1:A4',
+                                     {'type': 'icon_set',
+                                      'icon_style': '3_arrows',
+                                      'icons': [{'criteria': '>', 'type': 'percent', 'value': 0},
+                                                {'criteria': '<', 'type': 'percent', 'value': 0},
+                                                {'criteria': '>=', 'type': 'percent', 'value': 0}]})
+
+        workbook.close()
+
+        self.assertExcelEqual()
