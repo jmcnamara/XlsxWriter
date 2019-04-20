a17bf90bbd9a89a86bf1421f0309d0c722aa6f90
John McNamara <jmcnamara@cpan.org>
2019-04-20 17:33:59 +0100

    Prep for release 1.1.7

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class PyTest(Command):
 
 setup(
     name='XlsxWriter',
-    version='1.1.6',
+    version='1.1.7',
     author='John McNamara',
     author_email='jmcnamara@cpan.org',
     url='https://github.com/jmcnamara/XlsxWriter',
--- xlsxwriter/__init__.py
+++ xlsxwriter/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 __VERSION__ = __version__
 from .workbook import Workbook
