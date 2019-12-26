cad953fec5943c0ea61c7569014f4d9a38d902ae
John McNamara <jmcnamara@cpan.org>
2019-12-26 16:22:13 +0000

    Tests for duplicate images with in memory buffers.

---
+++ xlsxwriter/test/comparison/test_image_bytes04.py
@@ -0,0 +1,59 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2019, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+from io import BytesIO
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+
+        self.set_filename('image48.xlsx')
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file with image(s)."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet1 = workbook.add_worksheet()
+        worksheet2 = workbook.add_worksheet()
+
+        image_file = open(self.image_dir + 'red.png', 'rb')
+        image_data = BytesIO(image_file.read())
+        image_file.close()
+
+        worksheet1.insert_image('E9', 'red.png', {'image_data': image_data})
+        worksheet2.insert_image('E9', 'red.png', {'image_data': image_data})
+
+        workbook.close()
+
+        self.assertExcelEqual()
+
+    def test_create_file_in_memory(self):
+        """Test the creation of a simple XlsxWriter file with image(s)."""
+
+        workbook = Workbook(self.got_filename, {'in_memory': True})
+
+        worksheet1 = workbook.add_worksheet()
+        worksheet2 = workbook.add_worksheet()
+
+        image_file = open(self.image_dir + 'red.png', 'rb')
+        image_data = BytesIO(image_file.read())
+        image_file.close()
+
+        worksheet1.insert_image('E9', 'red.png', {'image_data': image_data})
+        worksheet2.insert_image('E9', 'red.png', {'image_data': image_data})
+
+        workbook.close()
+
+        self.assertExcelEqual()
---
+++ xlsxwriter/test/comparison/test_image_bytes05.py
@@ -0,0 +1,57 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2019, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+from io import BytesIO
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+
+        self.set_filename('image50.xlsx')
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file with image(s)."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        image_file = open(self.image_dir + 'red.png', 'rb')
+        image_data = BytesIO(image_file.read())
+        image_file.close()
+
+        worksheet.insert_image('E9', 'red.png', {'image_data': image_data})
+        worksheet.insert_image('E13', 'red.png', {'image_data': image_data})
+
+        workbook.close()
+
+        self.assertExcelEqual()
+
+    def test_create_file_in_memory(self):
+        """Test the creation of a simple XlsxWriter file with image(s)."""
+
+        workbook = Workbook(self.got_filename, {'in_memory': True})
+
+        worksheet = workbook.add_worksheet()
+
+        image_file = open(self.image_dir + 'red.png', 'rb')
+        image_data = BytesIO(image_file.read())
+        image_file.close()
+
+        worksheet.insert_image('E9', 'red.png', {'image_data': image_data})
+        worksheet.insert_image('E13', 'red.png', {'image_data': image_data})
+
+        workbook.close()
+
+        self.assertExcelEqual()
