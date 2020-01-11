a740ec53d101d97a9f883cd784dc87e4bc307fc8
John McNamara <jmcnamara@cpan.org>
2020-01-11 15:57:02 +0000

    Added additional tests.

---
+++ xlsxwriter/test/comparison/test_comment16.py
@@ -0,0 +1,43 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2019, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparsion_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+
+        self.set_filename('comment16.xlsx')
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file with comments."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        worksheet.write('A1', 'Foo')
+        worksheet.write('C7', 'Bar')
+        worksheet.write('G14', 'Baz')
+
+        worksheet.write_comment('A1', 'Some text')
+        worksheet.write_comment('D1', 'Some text')
+        worksheet.write_comment('C7', 'Some text')
+        worksheet.write_comment('E10', 'Some text')
+        worksheet.write_comment('G14', 'Some text')
+
+        worksheet.set_comments_author('John')
+
+        workbook.close()
+
+        self.assertExcelEqual()
