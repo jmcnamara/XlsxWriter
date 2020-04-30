049f7acaca4d91307d641fd52c588137e47d5b4e
John McNamara <jmcnamara@cpan.org>
2020-04-30 23:56:33 +0100

    Test case for issue gh709.
    Issue #709

---
+++ xlsxwriter/test/comparison/test_image52.py
@@ -0,0 +1,33 @@
+###############################################################################
+#
+# Tests for XlsxWriter.
+#
+# Copyright (c), 2013-2020, John McNamara, jmcnamara@cpan.org
+#
+
+from ..excel_comparison_test import ExcelComparisonTest
+from ...workbook import Workbook
+
+
+class TestCompareXLSXFiles(ExcelComparisonTest):
+    """
+    Test file created by XlsxWriter against a file created by Excel.
+
+    """
+
+    def setUp(self):
+
+        self.set_filename('image52.xlsx')
+
+    def test_create_file(self):
+        """Test the creation of a simple XlsxWriter file with image(s)."""
+
+        workbook = Workbook(self.got_filename)
+
+        worksheet = workbook.add_worksheet()
+
+        worksheet.insert_image('B2', self.image_dir + 'gh709.png')
+
+        workbook.close()
+
+        self.assertExcelEqual()
