3609915a8e9017715b27e7aec65deeaa996f7808
John McNamara <jmcnamara@cpan.org>
2021-04-15 00:43:25 +0100

    Prep for release 1.3.9

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class PyTest(Command):
 
 setup(
     name='XlsxWriter',
-    version='1.3.8',
+    version='1.3.9',
     author='John McNamara',
     author_email='jmcnamara@cpan.org',
     url='https://github.com/jmcnamara/XlsxWriter',
