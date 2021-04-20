b6d88061ca2b5ca043dc08b203b48017fb6ea7a7
John McNamara <jmcnamara@cpan.org>
2021-04-20 16:30:50 +0100

    Add minor clarification to hide_sheet example.

--- examples/hide_sheet.py
+++ examples/hide_sheet.py
@@ -22,4 +22,11 @@ worksheet1.write('A1', 'Sheet2 is hidden')
 worksheet2.write('A1', "Now it's my turn to find you!")
 worksheet3.write('A1', 'Sheet2 is hidden')
 
+# Note, you can't hide the the "active" worksheet, which generally is the
+# first worksheet, since this would cause an Excel error. So, in order to hide
+# the first sheet you will need to activate another worksheet:
+#
+#    worksheet2.activate()
+#    worksheet1.hide()
+
 workbook.close()
