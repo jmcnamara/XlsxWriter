34faecc0041009cefade3261a9d17f1f2adb22ed
John McNamara <jmcnamara@cpan.org>
2021-05-07 06:49:14 +0100

    Prep for release 1.4.2

--- dev/docs/source/conf.py
+++ dev/docs/source/conf.py
@@ -8,7 +8,7 @@ master_doc = 'index'
 project = u'XlsxWriter'
 copyright = u'2013-2021, John McNamara'
 
-version = '1.4.1'
+version = '1.4.2'
 release = version
 
 exclude_patterns = []
--- xlsxwriter/__init__.py
+++ xlsxwriter/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 __VERSION__ = __version__
 from .workbook import Workbook
