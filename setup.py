ce709498162f9e94bf5ae5cd99b268f66afbec36
John McNamara <jmcnamara@cpan.org>
2021-08-11 21:00:27 +0100

    Fix min python version in warning.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup, Command
 
 if sys.version_info < (3, 4, 0):
-    warn("The minimum Python version supported by XlsxWriter is 3.4.2")
+    warn("The minimum Python version supported by XlsxWriter is 3.4.0")
     exit()
 
 
