124bbaa311b7a9cf657a4f903e0b28aacaca5eaa
Agriya Khetarpal <agriyakhetarpal@outlook.com>
2022-06-26 23:24:01 +0530

    Updated xl_col_to_name() method to add recursive method

--- xlsxwriter/utility.py
+++ xlsxwriter/utility.py
@@ -80,32 +80,35 @@ def xl_col_to_name(col, col_abs=False):
         Column style string.
 
     """
+    alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     col_num = col
     if col_num < 0:
         warn("Col number %d must be >= 0" % col_num)
         return None
 
-    col_num += 1  # Change to 1-index.
-    col_str = ''
     col_abs = '$' if col_abs else ''
 
-    while col_num:
-        # Set remainder from 1 .. 26
-        remainder = col_num % 26
+    # Return the alphabet if the index is less than 26
+    if col_num < 26:
+        return col_abs + alphabets[col_num]
 
-        if remainder == 0:
-            remainder = 26
+    # If index is greater than or equal to 26
+    else:
+        # Store the quotient, remainder in a tuple via the divmod() method
+        index = divmod(col_num, 26)
 
-        # Convert the remainder to a character.
-        col_letter = chr(ord('A') + remainder - 1)
+    # If remainder is 0
+    if index[1] == 0:
+        # If quotient equals 1, return the alphabet
+        if index[0] == 1:
+            return col_abs + alphabets[index[0] - 1] + alphabets[index[1]]
+        else:
+            # If the quotient > 1 then use recursive relation
+            return col_abs + xl_col_to_name(index[0] - 1) + alphabets[index[1]]
 
-        # Accumulate the column letters, right to left.
-        col_str = col_letter + col_str
-
-        # Get the next order of magnitude.
-        col_num = int((col_num - 1) / 26)
-
-    return col_abs + col_str
+    # If remainder is not 0
+    else:
+        return col_abs + xl_col_to_name(index[0] - 1) + alphabets[index[1]]
 
 
 def xl_cell_to_rowcol(cell_str):
