b4ab117b02f657823c9de96c305c900f257bbdf2
John McNamara <jmcnamara@cpan.org>
2022-12-28 16:07:29 +0000

    Prep for release 3.0.4

--- xlsxwriter/__init__.py
+++ xlsxwriter/__init__.py
@@ -2,6 +2,6 @@
 # SPDX-License-Identifier: BSD-2-Clause
 # Copyright 2013-2022, John McNamara, jmcnamara@cpan.org
 #
-__version__ = '3.0.3'
+__version__ = '3.0.4'
 __VERSION__ = __version__
 from .workbook import Workbook
