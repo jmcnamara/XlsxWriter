8455ff07ddfec8a855743b2adfe0b4419c24e3e9
bcapuano <bcapuano@asu.edu>
2023-11-04 15:39:36 -0400

    Update fuzz path

--- xlsxwriter/fuzzing/build.sh
+++ xlsxwriter/fuzzing/build.sh
@@ -19,7 +19,7 @@ cd "$SRC"/XlsxWriter
 pip3 install .
 
 # Build fuzzers in $OUT
-for fuzzer in $(find src/fuzzing -name '*_fuzzer.py');do
+for fuzzer in $(find xlsxwriter/fuzzing -name '*_fuzzer.py');do
   compile_python_fuzzer "$fuzzer"
 done
 zip -q $OUT/xlsx_fuzzer_seed_corpus.zip $SRC/corpus/*
