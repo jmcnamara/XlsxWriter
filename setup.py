058d12418876bcb91cb11eccc0bb7ff66d56e2e0
John McNamara <jmcnamara@cpan.org>
2025-04-18 09:14:39 +0100

    package: clean up setup.py file

--- setup.py
+++ setup.py
@@ -1,55 +1,35 @@
 import sys
-import subprocess
 from warnings import warn
 
-try:
-    from setuptools import setup, Command
-except ImportError:
-    from distutils.core import setup, Command
+from setuptools import setup
 
 if sys.version_info < (3, 8):
     warn("The minimum Python version supported by XlsxWriter is 3.8")
     sys.exit()
 
-
-class PyTest(Command):
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        errno = subprocess.call(['python',  '-m', 'unittest', 'discover'])
-        raise SystemExit(errno)
-
 setup(
-    name='XlsxWriter',
-    version='3.2.3',
-    author='John McNamara',
-    author_email='jmcnamara@cpan.org',
-    url='https://github.com/jmcnamara/XlsxWriter',
-    packages=['xlsxwriter'],
-    scripts=['examples/vba_extract.py'],
-    cmdclass={'test': PyTest},
-    license='BSD-2-Clause',
-    description='A Python module for creating Excel XLSX files.',
-    long_description=open('README.rst').read(),
+    name="xlsxwriter",
+    version="3.2.3",
+    author="John McNamara",
+    author_email="jmcnamara@cpan.org",
+    url="https://github.com/jmcnamara/XlsxWriter",
+    packages=["xlsxwriter"],
+    scripts=["examples/vba_extract.py"],
+    license="BSD-2-Clause",
+    description="A Python module for creating Excel XLSX files.",
+    long_description=open("README.rst", encoding="utf-8").read(),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.8",
 )
